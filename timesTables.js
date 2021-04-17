7a75aea39ad80b02c685f1ee5f401634b3df9c62
Maurizio Meluzzi <maurizio@nuvolagroup.com.au>
2021-04-18 00:07:37 +0200

    new version with sounds and countdown

--- timesTables.js
+++ timesTables.js
@@ -1,16 +1,32 @@
 const calcBox     = document.getElementById('calc-box');
 const resBox      = document.getElementById('res-box');
+
 const modalOk     = document.getElementById("modal-ok");
-const modalNok    = document.getElementById("modal-nok");
 const modalOkRes  = document.getElementById("ok-row-02");
+const modalNok    = document.getElementById("modal-nok");
 const modalNokRes = document.getElementById("nok-row-02");
 
-const lastTimesTable = 5;
+const modalTimeEnded    = document.getElementById("modal-time-ended");
+const modalTimeEndedRes = document.getElementById("te-row-02");
+
+const modalWelcome = document.getElementById("modal-welcome");
+
+
+const clockSound = document.createElement("audio");
+clockSound.src = 'sound/singleTick.mp3';
+clockSound.setAttribute("preload", "auto");
+clockSound.setAttribute("controls", "none");
+clockSound.style.display = "none";
+document.body.appendChild(clockSound);
+
+const lastTimesTable = 7;
 
 let result       = '';
 let resultDigits = 0;
 
 let givenDigits  = 0;
+
+let cellShutter;
 
 // ----------------------------------------------------------------
 // get buttons and bind them
@@ -25,6 +41,8 @@ for (let i = 0; i < digitBtns.length; i++) {
 // bind close modal event
 modalOk.addEventListener("click",  closeModal);
 modalNok.addEventListener("click", closeModal);
+modalTimeEnded.addEventListener("click", closeModal);
+modalWelcome.addEventListener("click", closeModal);
 
 
 // ----------------------------------------------------------------
@@ -56,19 +74,15 @@ function showDigits(digitValue) {
 }
 
 function checkResult() {
+    clearInterval(cellShutter);
+    // clockSound.load();
+
     if (resBox.innerHTML == result) {
-        // console.log('GIUSTO!!!!');
-        // alert('GIUSTO!!!!');
         modalOk.style.display = "block";
     }
     else {
-        // console.log('SBAGLIATO');
-        // alert('SBAGLIATO');
         modalNok.style.display = "block";
     }
-
-    //resetDigits();
-    //generateCalc();
 }
 
 function resetDigits() {
@@ -77,8 +91,11 @@ function resetDigits() {
 }
 
 function closeModal() {
-    modalOk.style.display  = "none";
-    modalNok.style.display = "none";
+    modalOk.style.display        = "none";
+    modalNok.style.display       = "none";
+    modalTimeEnded.style.display = "none";
+    modalWelcome.style.display   = "none";
+
     resetDigits();
     generateCalc();
 }
@@ -96,11 +113,44 @@ function generateCalc() {
     result = '' + (x*y);
     resultDigits = result.length;
 
-    modalOkRes.innerHTML  = x + 'x' + y + '=' + result;
-    modalNokRes.innerHTML = x + 'x' + y + '=' + result;
+    modalOkRes.innerHTML        = x + 'x' + y + '=' + result;
+    modalNokRes.innerHTML       = x + 'x' + y + '=' + result;
+    modalTimeEndedRes.innerHTML = x + 'x' + y + '=' + result;
+
+    startCountDown();
 }
 
+function startCountDown() {
+    // recover background of table
+    const countDownTds = document.querySelectorAll('.time-counter');
+    countDownTds.forEach( element => {
+        element.style.backgroundColor = '#FADECB';
+    });
+
+    // clockSound.load();
+    // clockSound.play();
+
+    let remainingTd = 10;
+    cellShutter = setInterval(
+        function() {
+            if (remainingTd == 0) {
+                clearInterval(cellShutter);
+                modalTimeEnded.style.display = "block";
+            }
+            else {
+                let tdId = 'time-counter-' + remainingTd;
+                // console.log(tdId);
+                // document.getElementById('time-counter-' + remainingTd).style.backgroundColor = '#F4C2DF';
+                document.getElementById(tdId).style.backgroundColor = '#F4C2DF';
+                clockSound.play();
+                remainingTd--;
+            }
+        },
+        1000
+    );
+}
 
 // ----------------------------------------------------------------
 // ENTRY POINT
-generateCalc();
+// generateCalc();
+modalWelcome.style.display = "block";
